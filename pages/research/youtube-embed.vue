301318d021b2d39797f51c5ebc254a198c9661db
ghofaralhasyim <abdul.ghofar@student.umn.ac.id>
2023-03-31 13:38:55 +0700

    fix cued yt-embed

--- pages/research/youtube-embed.vue
+++ pages/research/youtube-embed.vue
@@ -1,9 +1,7 @@
 <template>
 	<main class="site-main">
 		<div class="container">
-			<div>
-				<div ref="player"></div>
-			</div>
+			<div ref="playerContainer"></div>
 		</div>
 	</main>
 </template>
@@ -12,36 +10,31 @@
 export default {
 	data() {
 		return {
-			player: null
+			player: null,
+			id: 'AWVy2pMOB8c'
 		}
 	},
 	mounted() {
-		this.initPlayer()
-	},
-	methods: {
-		async initPlayer() {
-			// Load the YouTube Iframe API
-			const tag = document.createElement('script')
-			tag.src = 'https://www.youtube.com/iframe_api'
-			const firstScriptTag = await document.getElementsByTagName('script')[0]
-			firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
+		// Load the YouTube IFrame API asynchronously
+		const tag = document.createElement('script')
+		tag.src = 'https://www.youtube.com/iframe_api'
+		const firstScriptTag = document.getElementsByTagName('script')[0]
+		firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
 
-			// Create the YouTube player
-			this.player = new window.YT.Player(this.$refs.player, {
-				height: '720',
+		// Create a YouTube player object when the API is ready
+		window.onYouTubeIframeAPIReady = () => {
+			this.player = new window.YT.Player(this.$refs.playerContainer, {
+				height: '600',
 				width: '100%',
 				videoId: 'AWVy2pMOB8c',
-				playerVars: {
-					autoplay: 0,
-					modestbranding: 1,
-					rel: 0
-				},
 				events: {
-					onStateChange: this.handlePlayerStateChange
+					onStateChange: this.onPlayerStateChange.bind(this)
 				}
 			})
-		},
-		handlePlayerStateChange(event) {
+		}
+	},
+	methods: {
+		onPlayerStateChange(event) {
 			// Check if the video has ended
 			if (event.data === window.YT.PlayerState.ENDED) {
 				// Refresh the player by reloading the video
